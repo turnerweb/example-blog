f00c88157aeca7296095897028290a23556953fd
Stefan Turner <turner.stefan281@gmail.com>
2021-05-16 11:18:15 +0200

    Change text

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -22,10 +22,11 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image'
         return (
             <section className="hero">
                 <div className="hero__content">
-                    <h1 className="hero__title">Interessante Orte in Portugal</h1>
+                    <h1 className="hero__title">Sehenswerte & interessante Orte in Portugal</h1>
                     <GatsbyImage image={getImage(data.file)} className="hero__img" alt="" loading="eager" />
                     <p className="hero__text">
-                        Arcu et adipiscing et lacus. Diam arcu cursus a pulvinar lacinia non elit id. Sed fames id amet metus nisl lacus a tincidunt.
+                        Alle Artikel auf dieser Seite sind Dummytexte. Die Website wurde mit Gatsby entwickelt.
+                        Verwaltet wird der Content mit Forestry CMS.
                     </p>
                 </div>  
         </section> 
