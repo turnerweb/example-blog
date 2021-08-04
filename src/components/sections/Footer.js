7e2c2ae2c98ed5eb21b9220ff0f1cab1a1343a46
Stefan Turner <turner.stefan281@gmail.com>
2021-08-04 10:37:48 +0200

    Change footer url

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="footer">
             <p className="footer__text">
-                {new Date().getFullYear()} stefanturner.site
+                {new Date().getFullYear()} <a href="https://www.stefanturner.ch">stefanturner.ch</a>
             </p>
         </footer>
     )
